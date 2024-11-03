70efd75815732621c29e5ef7a4d9f6fcc03e6cc0
jeremyylimmm <jeremylim.0304@gmail.com>
2024-11-03 18:53:51 +1100

    Clear screen

--- src/renderer.rs
+++ src/renderer.rs
@@ -12,7 +12,10 @@ pub struct Renderer {
   cmd_allocators: [ID3D12CommandAllocator;FRAMES_IN_FLIGHT],
   cmd_lists: [ID3D12GraphicsCommandList;FRAMES_IN_FLIGHT],
   swapchain_fences: [u64;DXGI_MAX_SWAP_CHAIN_BUFFERS as _],
+  swapchain_rtvs: [D3D12_CPU_DESCRIPTOR_HANDLE;DXGI_MAX_SWAP_CHAIN_BUFFERS as _],
+  swapchain_buffers: Vec<ID3D12Resource>,
   swapchain: IDXGISwapChain3,
+  rtv_heap: DescriptorHeap,
   frame: usize
 }
 
@@ -110,7 +113,11 @@ impl Renderer {
         },
       };
 
-      return Ok(Renderer {
+      let mut rtv_heap=DescriptorHeap::new(&device, 1024, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, D3D12_DESCRIPTOR_HEAP_FLAG_NONE)?;
+
+      let rtvs: [DescriptorHandle;DXGI_MAX_SWAP_CHAIN_BUFFERS as _] = std::array::from_fn(|_|rtv_heap.alloc());
+
+      let mut renderer = Renderer {
         device,
         queue,
         fence_val: 0,
@@ -119,9 +126,16 @@ impl Renderer {
         cmd_allocators,
         cmd_lists,
         swapchain_fences: [0;DXGI_MAX_SWAP_CHAIN_BUFFERS as _],
+        swapchain_rtvs: std::array::from_fn(|i|rtv_heap.cpu_handle(rtvs[i])),
+        swapchain_buffers: Vec::new(),
         swapchain,
+        rtv_heap,
         frame: 0
-      });
+      };
+
+      renderer.init_swapchain_resources();
+
+      return Ok(renderer);
     }
   }
 
@@ -137,10 +151,19 @@ impl Renderer {
       cmd_allocator.Reset().unwrap();
       cmd_list.Reset(cmd_allocator, None).unwrap();
 
+      // Start recording commands
+
+      cmd_list.ResourceBarrier(&[transition_barrier(&self.swapchain_buffers[swapchain_index], D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET)]);
+
+      cmd_list.ClearRenderTargetView(self.swapchain_rtvs[swapchain_index], &[0.2, 0.3, 0.3, 1.0], None); 
+
+      cmd_list.ResourceBarrier(&[transition_barrier(&self.swapchain_buffers[swapchain_index], D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT)]);
+
+      // Submit command buffer
       cmd_list.Close().unwrap();
+      let submit: [Option<ID3D12CommandList>;1] = [ Some(cmd_list.cast::<ID3D12CommandList>().unwrap()) ];
 
       self.queue.Wait(&self.fence, self.swapchain_fences[swapchain_index]).unwrap();
-      let submit: [Option<ID3D12CommandList>;1] = [ Some(cmd_list.cast::<ID3D12CommandList>().unwrap()) ];
       self.queue.ExecuteCommandLists(&submit);
       self.frame_fences[self.frame] = self.signal();
 
@@ -151,6 +174,27 @@ impl Renderer {
 
       self.frame = (self.frame + 1) % FRAMES_IN_FLIGHT;
     };
+  }
+
+  fn init_swapchain_resources(&mut self) {
+    unsafe {
+      self.swapchain_buffers.clear();
+      let desc = self.swapchain.GetDesc().unwrap();
+
+      for i in 0..desc.BufferCount {
+        self.swapchain_buffers.push(
+          self.swapchain.GetBuffer(i).unwrap()
+        );
+
+        let rtv_desc = D3D12_RENDER_TARGET_VIEW_DESC {
+          ViewDimension: D3D12_RTV_DIMENSION_TEXTURE2D,
+          Format: desc.BufferDesc.Format,
+          ..Default::default()
+        };
+
+        self.device.CreateRenderTargetView(&self.swapchain_buffers[i as usize], Some(&rtv_desc), self.swapchain_rtvs[i as usize]);
+      }
+    }
   }
 
   fn signal(&mut self) -> u64 {
@@ -173,6 +217,23 @@ impl Renderer {
   }
 }
 
+fn transition_barrier(resource: &ID3D12Resource, state_before: D3D12_RESOURCE_STATES, state_after: D3D12_RESOURCE_STATES) -> D3D12_RESOURCE_BARRIER {
+  return D3D12_RESOURCE_BARRIER {
+    Type: D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+
+    Anonymous: D3D12_RESOURCE_BARRIER_0 {
+      Transition: std::mem::ManuallyDrop::new(D3D12_RESOURCE_TRANSITION_BARRIER {
+        pResource: unsafe{std::mem::transmute_copy(resource)},
+        Subresource: D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+        StateBefore: state_before,
+        StateAfter: state_after,
+      })
+    },
+      
+    ..Default::default()
+  };
+}
+
 impl Drop for Renderer {
   fn drop(&mut self) {
     self.wait_device_idle();
@@ -186,4 +247,99 @@ fn hwnd_size(window: HWND) -> (i32, i32) {
     rect.right - rect.left,
     rect.bottom - rect.top
   );
+}
+
+struct DescriptorHeap {
+  id: u8,
+  heap: ID3D12DescriptorHeap,
+  free_list: Vec<u32>,
+  generation: Vec<u32>,
+  stride: usize,
+  cpu_base: D3D12_CPU_DESCRIPTOR_HANDLE,
+  gpu_base: D3D12_GPU_DESCRIPTOR_HANDLE
+}
+
+type DescriptorHandle = u64;
+
+static NEXT_DESCRIPTOR_ID: std::sync::atomic::AtomicU8 = std::sync::atomic::AtomicU8::new(0);
+
+impl DescriptorHeap {
+  fn new(device: &ID3D12Device, capacity: u32, desc_type: D3D12_DESCRIPTOR_HEAP_TYPE, flags: D3D12_DESCRIPTOR_HEAP_FLAGS) -> std::result::Result<DescriptorHeap, &'static str> {
+    unsafe{
+      let heap_desc = D3D12_DESCRIPTOR_HEAP_DESC {
+        Type: desc_type,
+        NumDescriptors: capacity,
+        Flags: flags,
+        ..Default::default()
+      };
+
+      let heap: ID3D12DescriptorHeap = match device.CreateDescriptorHeap(&heap_desc) {
+        Ok(heap) => {heap},
+        Err(_) => { return Err("Failed to create descriptor heap"); }
+      };
+
+      return Ok(DescriptorHeap {
+        id: NEXT_DESCRIPTOR_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed),
+        cpu_base: heap.GetCPUDescriptorHandleForHeapStart(),
+
+        gpu_base: if (flags & D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE) != D3D12_DESCRIPTOR_HEAP_FLAG_NONE {
+          heap.GetGPUDescriptorHandleForHeapStart()
+        }
+        else {
+          D3D12_GPU_DESCRIPTOR_HANDLE{..Default::default()}
+        },
+
+        heap,
+        free_list: (0..capacity).collect(),
+        generation: vec![1;capacity as usize],
+        stride: device.GetDescriptorHandleIncrementSize(desc_type) as usize,
+      });
+    }
+  }
+
+  fn alloc(&mut self) -> DescriptorHandle {
+    let index = self.free_list.pop().expect("Descriptor heap exhausted");
+    let gen = self.generation[index as usize];
+    return (((gen | ((self.id as u32) << 24)) as u64) << 32) | index as u64;
+  }
+
+  fn unsafe_handle_index(handle: DescriptorHandle) -> usize {
+    return (handle & 0xffffffff) as usize;
+  }
+
+  fn handle_metadata(handle: DescriptorHandle) -> (u32, u8) {
+    let shifted = (handle >> 32) as u32;
+    return (shifted & 0xffffff, (shifted >> 24) as u8);
+  }
+
+  fn verify_handle(&self, handle: DescriptorHandle) -> usize {
+    let index = Self::unsafe_handle_index(handle);
+    let (gen, id) = Self::handle_metadata(handle);
+
+    std::assert!(index < self.generation.len());
+    std::assert!(id == self.id);
+    std::assert!(gen == self.generation[index]);
+
+    return index;
+  }
+
+  fn free(&mut self, handle: DescriptorHandle) {
+    let index = self.verify_handle(handle);
+    self.generation[index] += 1; // Kill the handle
+    self.free_list.push(index as u32);
+  }
+
+  fn cpu_handle(&self, handle: DescriptorHandle) -> D3D12_CPU_DESCRIPTOR_HANDLE {
+    let index = self.verify_handle(handle);
+    return D3D12_CPU_DESCRIPTOR_HANDLE {
+      ptr: self.cpu_base.ptr + index * self.stride
+    };
+  }
+
+  fn gpu_handle(&self, handle: DescriptorHandle) -> D3D12_GPU_DESCRIPTOR_HANDLE {
+    let index = self.verify_handle(handle);
+    return D3D12_GPU_DESCRIPTOR_HANDLE {
+      ptr: self.gpu_base.ptr + (index * self.stride) as u64
+    };
+  }
 }
